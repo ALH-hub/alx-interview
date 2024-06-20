7ad8b06cc9222e0d757c131932bfac64e65c33ca
Alhadji Oumate <alhadjioum0@gmail.com>
2024-06-20 21:25:01 +0100

    fewest number of coings

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# determine the fewest number of coins needed to meet a given amount total.
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if coin <= total:
+            count += total // coin
+            total = total % coin
+    if total == 0:
+        return count
+    return -1

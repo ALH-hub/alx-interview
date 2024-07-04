c0b507f4f9999ef3bdc405aaf390f416b8216bcd
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-04 16:32:01 +0100

    prime game test

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,22 +1,46 @@
 #!/usr/bin/python3
-"""prime game module"""
+"""prime number game"""
 
 
-def isWinner(x, nums):
- """Determine who the winner of the game is. Maria or Ben."""
- maria_wins = 0
- ben_wins = 0
- for num in nums:
-  if num % 2 == 0:
-    ben_wins += 1
-  else:
-    if num > 3:
-      maria_wins += 1
-    else:
-      pass
-  if maria_wins > ben_wins:
-    return "Maria"
-  elif ben_wins > maria_wins:
-    return "Ben"
-  else:
-    return None
+def is_winner(x, nums):
+    """
+    Determines the winner of the game based on prime number selection.
+
+    Args:
+        x: Number of rounds played.
+        nums: List of integers representing the starting sets for each round.
+
+    Returns:
+        str: Name of the player who wins the most rounds ("Maria" or "Ben").
+        None: If the winner cannot be determined (a tie).
+    """
+
+    if not nums or x < 1:
+        return None
+
+    n = max(nums)
+    sieve = [1] * (n + 1)
+    sieve[0] = sieve[1] = 0
+
+    for i in range(2, int(n**0.5) + 1):
+        if sieve[i] == 1:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = 0
+
+    primes = [i for i in range(n + 1) if sieve[i] == 1]
+    primes_count = [0] * (n + 1)
+
+    count = 0
+    for i in range(1, n + 1):
+        if i in primes:
+            count += 1
+        primes_count[i] = count
+
+    ben_wins = 0
+    for num in nums:
+        ben_wins += primes_count[num] % 2 == 0
+
+    if ben_wins * 2 == x:
+        return None
+
+    return "Ben" if ben_wins > x // 2 else "Maria"
